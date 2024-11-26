5171e923fab63fb1b2666d255bd12ca178f71b15
bhanusanghi <bhanussanghi@gmail.com>
2024-11-26 18:47:55 +0530

    failing shareable script

--- script/OBSwap.s.sol
+++ script/OBSwap.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity =0.8.19;
 
 import {Surl} from "surl/Surl.sol";
 import {Script, console} from "forge-std/Script.sol";
@@ -7,15 +7,18 @@ import "forge-std/StdJson.sol";
 import "forge-std/console.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {Swapper, SingleTokenSwap} from "../src/Swapper.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract SwapUsingOBRouter is Script {
+    using SafeERC20 for IERC20;
     using Strings for uint256;
     using Surl for string;
     using stdJson for string;
     using Strings for address;
 
     string apiKey = vm.envString("OOGABOOGA_API_KEY");
-    address public swapper = 0x;
+    address public swapper = 0x41Dc9715f3c6Ed6f9A30c5D7404DDF93d67fC5ED;
     uint256 pk = vm.envUint("DEPLOYER_PRIVATE_KEY");
     address pkAddress = vm.addr(pk);
     //For OB Router Range from 0 to 1 to allow for price slippage
@@ -69,24 +72,45 @@ contract SwapUsingOBRouter is Script {
 
     function run() external {
         address yeet = 0x1740F679325ef3686B2f574e392007A92e4BeD41;
-        address wBera = 0x7507c1dc16935B82698e4C63f2746A2fCf994dF8;
+        address honey = 0x0E4aaF1351de4c0264C5c7056Ef3777b41BD8e03;
         uint inputAmount = 1 ether;
         // get current block number
         console.log("block number: ", block.number);
 
         // ######## reward harvest data ########
         // obero
-        (uint inputAmount,uint256 outputQuote,uint256 outputMin,address executor, string memory path) = getSwapData(wBera, yeet, inputAmount, pkAddress);
+        (
+            ,
+            uint256 outputQuote,
+            uint256 outputMin,
+            address executor,
+            string memory path
+        ) = getSwapData(yeet, honey, inputAmount, pkAddress);
 
         SingleTokenSwap memory swapData = SingleTokenSwap({
             inputAmount: inputAmount,
             outputQuote: outputQuote,
             outputMin: outputMin,
             executor: executor,
-            path: path
+            path: vm.parseBytes(path)
         });
+        // approve honey to be spent by swapper
+        console.log(
+            "honey balance: ",
+            IERC20(yeet).balanceOf{gas: 100000}(pkAddress)
+        );
+        vm.startBroadcast(pk);
+        IERC20(yeet).safeApprove(swapper, inputAmount);
 
-        Swapper swapper = Swapper(payable(swapper));
-        uint256 amountOut = swapper.swapTokens(swapData, wBera, yeet, pkAddress);
+        // swap honey for yeet
+        uint256 amountOut = Swapper(payable(swapper)).swapTokens(
+            swapData,
+            yeet,
+            honey,
+            pkAddress
+        );
+        vm.stopBroadcast();
+        console.log("amountOut: ", amountOut);
     }
 }
+// forge script script/OBSwap.s.sol --rpc-url bartio --broadcast --ffi
--- src/Swapper.sol
+++ src/Swapper.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.19;
 
 import {IOBRouter} from "./IOBRouter.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 struct SingleTokenSwap {
     uint256 inputAmount;
@@ -13,8 +14,10 @@ struct SingleTokenSwap {
 }
 
 contract Swapper {
-    uint referralCode = 0;
-    address public swapRouter = 0xF6eDCa3C79b4A3DFA82418e278a81604083b999D;
+    using SafeERC20 for IERC20;
+    uint32 referralCode = 0;
+    IOBRouter public swapRouter =
+        IOBRouter(0xF6eDCa3C79b4A3DFA82418e278a81604083b999D);
 
     function swapTokens(
         SingleTokenSwap calldata swapData,
